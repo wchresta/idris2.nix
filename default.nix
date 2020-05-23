d5409992454bdf77b45cca1d47989da5372359e7
wchresta <34962284+wchresta@users.noreply.github.com>
2020-05-23 12:48:59 -0400

    Bring in line with nixpkgs style
    * This derivation is in review to be included in nixpkgs

--- default.nix
+++ default.nix
@@ -1,14 +1,6 @@
 let
   pkgs = import <nixpkgs> { };
-  fetchFromGitHub = pkgs.fetchFromGitHub;
-  mkDerivation = pkgs.stdenv.mkDerivation;
-  lib = pkgs.stdenv.lib;
-  makeWrapper = pkgs.makeWrapper;
-  clang = pkgs.clang;
-  chez = pkgs.chez;
 in rec {
-  idris2 = import ./idris2.nix {
-    inherit mkDerivation fetchFromGitHub makeWrapper lib clang chez;
-  };
+  idris2 = pkgs.callPackage ./idris2.nix { };
 }
 
--- idris2.nix
+++ idris2.nix
@@ -1,29 +1,11 @@
-{ mkDerivation, fetchFromGitHub, lib, makeWrapper
+{ stdenv, fetchFromGitHub, makeWrapper
 , clang, chez
 }:
 
 # Uses scheme to bootstrap the build of idris2
-
-mkDerivation {
+stdenv.mkDerivation {
   name = "idris2";
-  version = "0.2.0";
-
-  nativeBuildInputs = [ makeWrapper ];
-  buildInputs = [ clang chez ];
-
-  prePatch = ''
-    patchShebangs --build tests
-  '';
-
-  makeFlags = "PREFIX=$(out)";
-
-  # The name of the main executable of chez is `scheme`
-  buildFlags = ''bootstrap SCHEME=scheme'';
-
-  # idris2 needs to find scheme at runtime to compile
-  postInstall = ''
-    wrapProgram "$out/bin/idris2" --prefix PATH : "${chez}/bin"
-  '';
+  version = "0.2.0-840e020";
 
   src = fetchFromGitHub {
     owner = "idris-lang";
@@ -32,11 +14,32 @@ mkDerivation {
     sha256 = "1l6pdjiglwd13pf56xwzbjzyyxgz48ypfggjgsgqk2w57rmbfy90";
   };
 
+  strictDeps = true;
+  nativeBuildInputs = [ makeWrapper clang chez ];
+  buildInputs = [ chez ];
+
+  prePatch = ''
+    patchShebangs --build tests
+
+    # Do not run tests as part of the build process
+    substituteInPlace bootstrap.sh --replace "make test" "# make test"
+  '';
+
+  makeFlags = [ "PREFIX=$(out)" ];
+
+  # The name of the main executable of pkgs.chez is `scheme`
+  buildFlags = [ "bootstrap" "SCHEME=scheme" ];
+
+  # idris2 needs to find scheme at runtime to compile
+  postInstall = ''
+    wrapProgram "$out/bin/idris2" --prefix PATH : "${chez}/bin"
+  '';
+
   meta = {
-    description = "Idris2";
+    description = "A purely functional programming language with first class types";
     homepage = https://github.com/idris-lang/Idris2;
-    license = lib.licenses.mit;
-    maintainers = [ ];
+    license = stdenv.lib.licenses.bsd3;
+    maintainers = with stdenv.lib.maintainers; [ wchresta ];
   };
 }
 
